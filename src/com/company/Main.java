91e0c59f1f6a517b789686b0769cd18df1891ef6
a4ko <a4k0o@protonmail.com>
2022-01-07 18:33:44 +0200

    Machine 1 test commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-
+//
         //TODO use String instead of Room
 
         Room[] hotel = new Room[10];
